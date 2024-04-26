0fa30080383f958d925911d1d949104522abf6cf
Mattjava <77417435+Mattjava@users.noreply.github.com>
2024-04-26 19:34:18 -0400

    Added comments and do some bug fixing

--- generator.py
+++ generator.py
@@ -1,9 +1,11 @@
 import random
 
+# Generates a random lowercase letter as a character.
 def generateLetter():
     asciiNum = random.randint(97, 122)
     return chr(asciiNum)
 
+# Generates a string using a given length and a random amount of characters.
 def generateString(length):
     newStr = ""
 
--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from generator import generateString
 from flask import Flask, render_template, url_for, request
 
+# Generates username and password
 username = generateString(8)
 password = generateString(10)
 
+# This number counts down when a user enters the incorrect details
+# When it reaches 0 or less, it will tell the user the password through the website
 count = 3
 
-print(username)
-print(password)
-
+# Checks if the inputted password is the correct password
+#Returns true if it is, and false if otherwise
 def checkPassword(input):
     if input == password:
         return True
     else:
         return False
 
-
+# Creates the web app
 app = Flask(__name__, template_folder="templates")
 
+# Connects the main method to the web app
 @app.route("/", methods=["GET", "POST"])
 def main():
     global count
     if request.method == "GET":
+        # Returns and generates "index.html" in its initial state
         return render_template("index.html", username=username, correct=True, count=count, password=password)
     elif request.method == "POST":
         if request.form["username"] != username or not checkPassword(request.form["password"]):
+            # Returns "index.html", but edited to show a message letting the user know they got something incorrect.
+            # It also subtracts 1 from count. Scroll up for more information.
             count += -1
             return render_template("index.html", username=username, correct=False, count=count, password=password)
         else:
+            # Returns and generates "correct.html"
             return render_template("correct.html")
 
 if __name__ == "__main__":
